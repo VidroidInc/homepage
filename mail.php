7117e4df5c679e7db0a1048a926c4c2fd651f39e
Jose Otavio Miguel <jose.miguel@fh.com.br>
2019-08-15 11:06:35 -0300

    envio

---
+++ mail.php
@@ -0,0 +1,36 @@
+<?php
+if(isset( $_POST['name']))
+  $name = $_POST['name'];
+if(isset( $_POST['email']))
+  $email = $_POST['email'];
+if(isset( $_POST['message']))
+  $message = $_POST['message'];
+if(isset( $_POST['subject']))
+  $subject = $_POST['subject'];
+if ($name === ''){
+  echo "Nome não pode estar vazio";
+  die();
+}
+if ($email === ''){
+  echo "Email não pode estar vazio";
+  die();
+} else {
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+    echo "Email invalido.";
+    die();
+  }
+}
+if ($subject === ''){
+  echo "Assunto não pode estar vazio";
+  die();
+}
+if ($message === ''){
+  echo "Mensagem não pode estar vazia.";
+  die();
+}
+$content="From: $name \nEmail: $email \nMessage: $message";
+$recipient = "contato@vidroid.com.br";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $content, $mailheader) or die("Error!");
+echo "Email enviado!";
+?>
